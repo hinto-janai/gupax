a07f18d2decb1be6152499505ed6bb06d342e90e
hinto.janai <hinto.janai@protonmail.com>
2023-03-01 12:13:26 -0500

    node: add `p2pool.uk` (good uptime)

--- src/node.rs
+++ src/node.rs
@@ -34,7 +34,7 @@ use hyper::{
 // Remote Monero Nodes with ZMQ enabled, sourced from: [https://github.com/hinto-janai/monero-nodes]
 // The format is an array of tuples consisting of: (IP, LOCATION, RPC_PORT, ZMQ_PORT)
 
-pub const REMOTE_NODES: [(&str, &str, &str, &str); 22] = [
+pub const REMOTE_NODES: [(&str, &str, &str, &str); 23] = [
 	("monero.10z.com.ar",          "AR - Buenos Aires F.D.",         "18089", "18084"),
 	("escom.sadovo.com",           "BG - Plovdiv",                   "18089", "18084"),
 	("monero2.10z.com.ar",         "BR - São Paulo",                 "18089", "18083"),
@@ -46,6 +46,7 @@ pub const REMOTE_NODES: [(&str, &str, &str, &str); 22] = [
 	("p2pmd.xmrvsbeast.com",       "DE - Hesse",                     "18081", "18083"),
 	("reynald.ro",                 "FR - Île-de-France",             "18089", "18084"),
 	("node2.monerodevs.org",       "FR - Occitanie",                 "18089", "18084"),
+	("p2pool.uk",                  "GB - England",                   "18089", "18084"),
 	("monero.homeqloud.com",       "GR - East Macedonia and Thrace", "18089", "18083"),
 	("home.allantaylor.kiwi",      "NZ - Canterbury",                "18089", "18083"),
 	("ru.poiuty.com",              "RU - Kuzbass",                   "18081", "18084"),
