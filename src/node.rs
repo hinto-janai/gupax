f156d7b9a2fd29f6653c7ffc034b382b0d8a4855
hinto.janai <hinto.janai@protonmail.com>
2023-10-22 17:15:01 -0400

    remove node: `oracle.netrix.cc`

--- src/node.rs
+++ src/node.rs
@@ -34,11 +34,10 @@ use hyper::{
 // Remote Monero Nodes with ZMQ enabled, sourced from: [https://github.com/hinto-janai/monero-nodes]
 // The format is an array of tuples consisting of: (IP, LOCATION, RPC_PORT, ZMQ_PORT)
 
-pub const REMOTE_NODES: [(&str, &str, &str, &str); 20] = [
+pub const REMOTE_NODES: [(&str, &str, &str, &str); 19] = [
 	("monero.10z.com.ar",           "AR - Buenos Aires F.D.",         "18089", "18084"),
 	("monero1.heitechsoft.com",     "CA - Ontario",                   "18081", "18084"),
 	("node.monerodevs.org",         "CA - Quebec",                    "18089", "18084"),
-	("oracle.netrix.cc",            "CH - Zurich",                    "18089", "18083"),
 	("node.cryptocano.de",          "DE - Lower Saxony",              "18089", "18083"),
 	("p2pmd.xmrvsbeast.com",        "DE - Hesse",                     "18081", "18083"),
 	("fbx.tranbert.com",            "FR - Île-de-France",             "18089", "18084"),
